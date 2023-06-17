dfe4f6c7c98329a0e27353021491bfd71596eac9
Tanmay Agrawal <tanmayagrawal2004@gmail.com>
2023-06-17 22:31:38 +0530

    Outline created

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+
+    <h1> To Do List </h1>
+    <!-- each task encapsulated in a box with option to delete -->
+    <ListItem v-for="(task, index) in tasks" :key="index" :task="task.task" :subtasks="task.subtasks" class="list-item"/>
+
+    <!-- Add new list item-->
+    <div class="list-item">
+      <div class="list-item__title">
+        <input type="text" placeholder="Add new task" />
+      </div>
+      <div class="list-item__subtitle">
+        <input type="text" placeholder="Add new subtask" />
+      </div>
+    </div>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ListItem from './components/ListItem.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ListItem
+  }, 
+  data() {
+    return {
+      tasks: [
+        {
+          task: 'Task 1',
+          subtasks: [
+            'Subtask 1',
+            'Subtask 2',
+            'Subtask 3']
+        },
+        {
+          task: 'Task 2',
+          subtasks: [
+            'Subtask 1',
+            'Subtask 2',
+            'Subtask 3']
+        },
+        {
+          task: 'Task 3',
+          subtasks: [
+          ]
+        }
+      ]
+    }
   }
 }
 </script>
@@ -22,5 +62,32 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  margin: 0 auto;
 }
+
+.list-item {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  margin-top: 2vh;
+  margin-left: auto;
+  display: inline-block;
+  width: 100%;
+}
+
+.list-item__title {
+  font-size: 1.5em;
+  font-weight: bold;
+  padding: 10px;
+  border-bottom: 1px solid #ccc;
+  text-align: left;
+  padding-left: 5vw;
+}
+
+.list-item__subtitle {
+  display: inline-block;
+  width: 100%;
+  margin-left: 0;
+  padding-left: 0;
+}
+
 </style>
---
+++ src/components/ListItem.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="list-item">
+        <!-- Task -->
+        <div class="list-item__title">
+            {{ task }}
+        </div>
+        <!-- subtasks : user editable or GPT generatable-->
+        <div class="list-item__subtitle">
+            <!-- iterate through subtasks-->
+            <ol>
+                <v-btn variant="outlined" v-if = "subtasks.length == 0">Generate subtasks</v-btn>
+                <div v-else>
+                    <li v-for="(subtask, index) in subtasks" :key="index">
+                        {{ subtask }}
+                    </li>
+                </div>
+            </ol>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ListItem',
+    props: {
+        task: String,
+        subtasks: String
+    }
+}
+</script>
+
+<style scoped>
+/* make the ol numbers appear next to the task */
+.list-item__subtitle {
+    display: inline-block;
+    width: 100%;
+    margin-left: 0;
+    padding-left: 0;
+}
+</style>
